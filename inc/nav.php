9ff797e00554b3209957d23c9c065e2355e6c12e
Huanggs <nightidea@gmail.com>
2014-08-11 14:41:37 +0800

    qq_08111441
    增加导航链接及文件

--- inc/nav.php
+++ inc/nav.php
@@ -1,8 +1,8 @@
 		<div id="nav">
 			<ul>
-				<li><a href="#">首页</a></li>
-				<li><a href="#">文章列表</a></li>
-				<li><a href="#">个人资料</a></li>
-				<li><a href="#">相册</a></li>
+				<li><a href="index.php">首页</a></li>
+				<li><a href="list.php">文章列表</a></li>
+				<li><a href="pro.php">个人资料</a></li>
+				<li><a href="album.php">相册</a></li>
 			</ul>
 		</div>
