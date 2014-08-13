2bb8341eee0ac0a09353cf54ce303809daff0326
Huanggs <nightidea@gmail.com>
2014-08-13 15:15:13 +0800

    office_08131515_bootstrap

--- index.php
+++ index.php
@@ -1,23 +1,25 @@
 <?php include 'inc/conn.php'; ?>
 <!DOCTYPE html>
 <head>
-	<meta charset="utf-8" />
-	<title>首页 - <?=$blog_name?></title>
-	<link rel="stylesheet" type="text/css" href="css/main.css">
+  <meta charset="utf-8" />
+  <title>首页 - <?=$blog_name?></title>
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="js/bootstrap.min.js"></script>
 </head>
 
 <body>
-	<div class="main center">
-		<?php include 'inc/top.php';?>
-		<?php include 'inc/nav1.php';?>
-		<div class="content center n_bg">
-			<?php while ($data_atcl_info=mysql_fetch_array($query_atcl_info)) {
-				include 'inc/atcl_list.php';
-				}
-			?>
-		</div>
-	</div>
+  <div class="main center">
+    <?php include 'inc/top.php';?>
+    <?php include 'inc/nav1.php';?>
+      <div class="content center n_bg">
+        <?php while ($data_atcl_info=mysql_fetch_array($query_atcl_info)) {
+          include 'inc/atcl_list.php';
+          }
+        ?>
+      </div>
+    </div>
 
-	<div class="footer center">2014 by GhostHuang</div>
+    <div class="footer center">2014 by GhostHuang</div>
 </body>
 </html>
